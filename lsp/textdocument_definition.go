57311a4d14c42e2444ea0fae6f1255f024a24a85
ArExi0n <notbollt@gmail.com>
2026-08-27 21:30:43 +0530

    begin working on hover

---
+++ lsp/textdocument_definition.go
@@ -0,0 +1,15 @@
+package lsp
+
+type DefinitonRequest struct {
+	Request
+	Params DefinitionParams `json:"params"`
+}
+
+type DefinitionParams struct {
+	TextDocumentPositionParams
+}
+
+type DefinitionResponse struct {
+	Response
+	Result []Location `json:"result"`
+}
